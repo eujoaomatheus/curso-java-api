98e017efcb6f8b92a44ab3e3b19d61de5cbefd58
João Matheus <joaomatheusgs7@gmail.com>
2024-04-05 16:32:03 -0300

    Implementação dos outros métodos do controller de Usuario

--- src/main/java/com/estudos/java/cursoapiv1/controllers/UserController.java
+++ src/main/java/com/estudos/java/cursoapiv1/controllers/UserController.java
@@ -2,12 +2,10 @@ package com.estudos.java.cursoapiv1.controllers;
 
 import com.estudos.java.cursoapiv1.entities.User;
 import com.estudos.java.cursoapiv1.services.UserService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +22,18 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<User>> listar() {
-
         return new ResponseEntity<>(userService.listar(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> buscarPeloId(@PathVariable Long id) {
+        User user = userService.buscarPeloId(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> salvar(@RequestBody User user) {
+        User usuario = userService.salvar(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
